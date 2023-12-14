b56cd44f1f063a668e8a1c8abf9f4b137c814ce7
Helen Oridupa <94105502+temlena@users.noreply.github.com>
2023-12-14 03:47:19 +0100

    Initial commit

---
+++ Display.js
@@ -0,0 +1,8 @@
+// Build a simple function that displays the following:console.log('THis is a text inside a function block'
+// Display a simple message outside a function that says, This is a text outside a function block
+
+function displayContent(){
+    console.log('THis is a text inside a function block')
+}
+displayContent() 
+console.log("This is a text outside a function block");
