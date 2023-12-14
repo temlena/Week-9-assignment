53f08bf5348d4fcb23340aecd8557407a9eb665b
Helen Oridupa <94105502+temlena@users.noreply.github.com>
2023-12-14 03:34:55 +0100

    Initial commit

---
+++ Printnumber.js
@@ -0,0 +1,5 @@
+// Build a simple loop that print numbers to the console from 0 to the length of an array.
+const array = [18, 20, 25, 30, 60, 73, 3, 45, 15, 12, 5, 22,10]
+for (let i = 0; i < array.length; i++){
+    console.log(i);
+}
